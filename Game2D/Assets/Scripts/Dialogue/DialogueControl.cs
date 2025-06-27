b2a5bf1006277237bd68c9455dde578dd4838ab0
Sabrina Notebook <sabrinaprogram99@gmail.com>
2025-06-27 09:23:43 -0300

    including dialogue code part 1

---
+++ Game2D/Assets/Scripts/Dialogue/DialogueControl.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueControl : MonoBehaviour
+{
+    [Header("Components")]
+    public GameObject dialogueObj; // janela do dialogo
+    public Image profileSprite; //sprite do perfil
+    public Text speachText; // texto da fala
+    public Text actorNameText; //nome do npc
+
+    [Header("Settings")]
+
+    public float typingSpeed; //velocidade da fala
+
+
+    //variaveis de controle
+    private bool isShowing; // se janela esta visivel
+    private int index; // index das sentenças, falas, textos
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
